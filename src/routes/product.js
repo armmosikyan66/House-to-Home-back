b08bff21fa04f04cad28987f1af210485e28cdff
armmosikyan66 <armanmosikyan@gmail.com>
2023-03-25 13:27:46 +0400

    optomize and create aggregator of classes

--- src/routes/product.js
+++ src/routes/product.js
@@ -7,6 +7,6 @@ router.post("/get-all/:lang/:page", ProductController.getAll);
 router.get('/uploads/:folder/:filename', ProductController.getImages);
 router.get("/get-one/:id", ProductController.getOnePrd);
 router.get("/get-recommended/:lang/:status", ProductController.getRecommended);
-router.get("/get-saved", ProductController.getSaved);
+router.post("/get-saved", ProductController.getSaved);
 
 export default router;
