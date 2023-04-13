e434bd800cc4d1882f25b180ccc9c49d0ad3a34b
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-13 22:22:27 +0400

    qweqwe

--- src/utils/uploader.js
+++ src/utils/uploader.js
@@ -4,12 +4,16 @@ import * as fs from "fs";
 import fileFilter from "./helpers/fileFilter.js";
 import generateUniqueNumber from "./helpers/generateUniqueNumber.js";
 
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+
 const generatedFolder = generateUniqueNumber(1, 999999).toString();
-const cwd = process.cwd();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const directoryPath = path.join(cwd, 'static', 'uploads', generatedFolder);
+        const directoryPath = path.join(__dirname, 'static', 'uploads', generatedFolder);
         fs.mkdir(directoryPath, {recursive: true}, (err) => cb(err, directoryPath));
     },
     filename: (req, file, cb) => {
@@ -18,7 +22,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({
-    dest: path.join(cwd, 'static', 'uploads'),
+    dest: path.join(__dirname, 'static', 'uploads'),
     storage,
     fileFilter,
     limits: {
