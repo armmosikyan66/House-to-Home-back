6935d12bf0cfc11405a5721a0f85051948a4767c
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-15 15:50:32 +0400

    qwe

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,13 @@ import validationChecker from "../middlewares/validationChecker.js";
 import UserController from "../controllers/user.controller.js";
 import checkAuth from "../middlewares/checkAuth.js";
 
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+import * as path from "path";
+
 const router = new Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 router.post("/register", authValidation, validationChecker, UserController.register);
 router.get('/activate/:link', UserController.activate)
@@ -14,5 +20,12 @@ router.post("/logout", UserController.logout);
 router.get('/refresh', UserController.refresh);
 router.post("/add-favorite", checkAuth, UserController.addFavorite);
 router.post("/remove-favorite", checkAuth, UserController.removeFavorite);
+router.post("/api/check", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        dirname: __dirname,
+        process: process.cwd()
+    })
+})
 
 export default router;
