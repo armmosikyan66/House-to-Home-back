f8eb7647b0c1756211232d3290d8fb9ab2cec818
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-16 16:13:26 +0400

    added cors dynamic options

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
-import  express from "express"
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import errorHandler from "./src/middlewares/errorHandler.js";
 import auth from "./src/routes/user.js";
-import "./src/database/db.js"
+import "./src/database/db.js";
 import product from "./src/routes/product.js";
-import {fileURLToPath} from "url";
-import {dirname} from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import * as path from "path";
 import admin from "./src/routes/admin.js";
 
@@ -17,26 +17,35 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const whitelist = [
+  "https://house-to-home-front.vercel.app",
+  "http://localhost:3000",
+];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions = { credentials: true };
+  console.log(req.header("Origin"));
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions.origin = true; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions.origin = false; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: "https://house-to-home-front.vercel.app"
-    // origin: "http://localhost:3000"
-}));
-app.use(express.static(path.join(__dirname, 'static')));
-app.use('/api', auth);
-app.use('/api', product);
-app.use('/api/admin', admin);
+app.use(cors(corsOptionsDelegate));
+app.use(express.static(path.join(__dirname, "static")));
+app.use("/api", auth);
+app.use("/api", product);
+app.use("/api/admin", admin);
 app.use((req, res) => {
-    res.status(404).send(`404: Page not found`);
+  res.status(404).send(`404: Page not found`);
 });
-  
+
 app.use(errorHandler);
 
 const start = async () => {
-    app.listen(PORT, ()=> console.log("\nlistening on port " + PORT))
-}
+  app.listen(PORT, () => console.log("\nlistening on port " + PORT));
+};
 
 start();
