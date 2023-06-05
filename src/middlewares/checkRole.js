0dab1abd36b333d04e4446022a1c687ea9570591
armmosikyan66 <armanmosikyan@gmail.com>
2023-06-05 23:46:00 +0400

    fixes

--- src/middlewares/checkRole.js
+++ src/middlewares/checkRole.js
@@ -1,24 +1,11 @@
-import {AccessClosed, AuthError} from "../exceptions/index.js";
-import {REQUIRED} from "../utils/constants.js";
-import TokenService from "../services/token.service.js";
+import {AccessClosed} from "../exceptions/index.js";
 
 export default function(roles)  {
     return (req, res, next,) => {
-        const authorizationHeader = req.headers.authorization;
+        const user = req.user;
 
-        if (!authorizationHeader) {
-            return next(new Error(REQUIRED("Authorization Header ")));
-        }
-
-        const accessToken = authorizationHeader.split(' ')[1];
-
-        if (!accessToken) {
-            return next(new AuthError("token not found"));
-        }
-
-        const userData = TokenService.validateAccessToken(accessToken);
-        if(!roles.includes(userData.role)) {
-            return next(new AccessClosed(`Role: ${userData.role} is not allowed`));
+        if(!roles.includes(user.role)) {
+            return next(new AccessClosed(`Role: ${user.role} is not allowed`));
         }
 
         next()
